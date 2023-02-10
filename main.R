5cb207c87336d0fa271b4c969addf91f49ecc277
Francesco <francesco.pasanisi@wur.nl>
2023-02-10 17:08:28 +0100

    add code to download data

---
+++ main.R
@@ -0,0 +1,15 @@
+#!/usr/bin/env Rscript
+
+# check if sf is installed an load it
+if(!"sf" %in% installed.packages()){install.packages("sf")}
+library(sf)
+
+# output_dir = 'output'
+# if (!dir.exists(outpit_dir)){
+#   dir.create(output_dir)
+# }
+
+
+source('R/getData.R')
+
+data_url <- getData('France', adm_level = 2, format = 'json')
